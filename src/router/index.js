f3798712023a26a1912b513c91affe9fc854aed9
Ghostenpower <2315181588@qq.com>
2024-12-29 05:17:54 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,6 @@ import OrderListView from '@/views/OrderListView.vue'
 import ProductDetailView from '@/views/ProductDetailView.vue'
 import LoginView from '@/views/LoginView.vue'
 import store from '@/store'
-import RegisterView from '@/views/RegisterView.vue'
 import { msgError, msgSuccess, msgWarning } from '@/utils/message'
 import ProductListView from '@/views/ProductListView.vue'
 import HomeView from '@/views/HomeView.vue'
@@ -101,11 +100,7 @@ const router = createRouter({
       name: 'login',
       component: LoginView
     },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView
-    }
+
   ]
 })
 
